abafa86751ab6a6a16a2ced46f98895fb78da94e
abdallahabusedo <abdallahsedo@hotmail.com>
2023-06-30 21:57:07 +0300

    list table request

--- src/components/HomePage/HomeComponents.jsx
+++ src/components/HomePage/HomeComponents.jsx
@@ -2,8 +2,26 @@ import { Box, Button, Text } from "@mantine/core";
 import React from "react";
 import TableComponent from "./Partials/Table/TableComponent";
 import HomeHeader from "./Partials/Header/HomeHeader";
+import axios from "axios";
 
 const HomeComponents = () => {
+  const [tableData, setTableData] = React.useState([]);
+  React.useEffect(() => {
+    const configuration = {
+      method: "get",
+      url: "http://localhost:3001/deposits",
+    };
+    // make the API call
+    axios(configuration)
+      .then((res) => {
+        console.log(res);
+        setTableData(res.data);
+      })
+      .catch((error) => {
+        // setErrorMessage(error.response.data.message);
+        console.log(error);
+      });
+  }, []);
   return (
     <Box
       sx={{
@@ -12,7 +30,7 @@ const HomeComponents = () => {
     >
       <HomeHeader />
       <Box>
-        <TableComponent />
+        <TableComponent tableData={tableData} />
       </Box>
     </Box>
   );
--- src/components/HomePage/Partials/Table/TableComponent.jsx
+++ src/components/HomePage/Partials/Table/TableComponent.jsx
@@ -3,46 +3,28 @@ import React from "react";
 import DateRow from "./TableRows/DateRow";
 import StringRow from "./TableRows/StringRow";
 
-let data = [
-  {
-    totalAmount: 30000,
-    currentDate: new Date().toLocaleString(),
-    goalDate: new Date().toLocaleString(),
-    monthlyPayment: 1250,
-  },
-  {
-    totalAmount: 30000,
-    currentDate: new Date().toLocaleString(),
-    goalDate: new Date().toLocaleString(),
-    monthlyPayment: 1250,
-  },
-  {
-    totalAmount: 30000,
-    currentDate: new Date().toLocaleString(),
-    goalDate: new Date().toLocaleString(),
-    monthlyPayment: 1250,
-  },
-];
-const TableComponent = () => {
-  const rows = data.map((item, index) => (
-    <tr key={index}>
-      <td>
-        <StringRow str={index + 1} />
-      </td>
-      <td>
-        <StringRow str={item.totalAmount} />
-      </td>
-      <td>
-        <DateRow date={item.currentDate} />
-      </td>
-      <td>
-        <DateRow date={item.goalDate} />
-      </td>
-      <td>
-        <StringRow str={item.monthlyPayment} />
-      </td>
-    </tr>
-  ));
+const TableComponent = (props) => {
+  const rows =
+    props.tableData &&
+    props.tableData.map((item, index) => (
+      <tr key={index}>
+        <td>
+          <StringRow str={index + 1} />
+        </td>
+        <td>
+          <StringRow str={item.totalAmount} />
+        </td>
+        <td>
+          <DateRow date={item.currentDate} />
+        </td>
+        <td>
+          <DateRow date={item.goalDate} />
+        </td>
+        <td>
+          <StringRow str={item.monthlyPayment} />
+        </td>
+      </tr>
+    ));
 
   return (
     <ScrollArea>
